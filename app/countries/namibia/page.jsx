ccb3495909abdeee1d8c251031176d061919cdff
PeterMExley <exley@mac.com>
2026-05-22 10:19:01 +0100

    Update page.jsx

--- app/countries/namibia/page.jsx
+++ app/countries/namibia/page.jsx
@@ -1,9 +1,4 @@
 "use client";
-
-export default function NamibiaPage() {
-  return (
-    <div>
-      <h1>Namibia<h1></h1>
-    </div>
-);
+export default function NamibiaPage(){
+  return Namibia;
 }
