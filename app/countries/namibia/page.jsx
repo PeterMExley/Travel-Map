738b5ce1db2b2dd66fbea4064a762d2ead5182c2
PeterMExley <exley@mac.com>
2026-05-22 10:29:53 +0100

    Update page.jsx

--- app/countries/namibia/page.jsx
+++ app/countries/namibia/page.jsx
@@ -2,14 +2,8 @@
 export default function NamibiaPage() {
 return (
 
-
 Namibia
-
-  <img
-    src="/photos/africa/namibia/dunes.jpeg"
-    alt="Namibia dunes"
-    className="w-full max-w-3xl rounded-2xl"
-  />
+  <img src="/photos/africa/namibia/dunes.jpeg" alt="Namibia dunes" className="w-full max-w-3xl rounded-2xl" />
 </div>
 );
 }
