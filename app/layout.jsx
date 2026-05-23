111f5a9c202d44fe45b961344da2ed3ca0a6d492
PeterMExley <exley@mac.com>
2026-05-23 11:56:13 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,10 @@
 import "./globals.css";
+
 export const metadata = {
   title: "Travel Map",
-  description: "Interactive travel archive",
+  description: "interactive travel archive",
 };
+
 export default function RootLayout({ children }) {
-  return (
-    
-    { children }
-    
-    ;
+  return { children };
 }
