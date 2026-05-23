d8c53e4649f3b0e7115be614ce0d55b04669b076
PeterMExley <exley@mac.com>
2026-05-23 11:20:01 +0100

    new

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+import "./globals.css";
+export const metadata = {
+  title: "Travel Map",
+  description: "Interactive travel archive",
+};
+export default function RootLayout({ children }) {
+  return { children };
+}
